bf7189abcd4796701f6b652368f99eeeaa6d0ef2
LuisRG-L <pequeluis14@gmail.com>
2024-08-15 17:50:03 +0200

    Fix --minus config

--- web/js/idgen.js
+++ web/js/idgen.js
@@ -1,3 +1,4 @@
+/*
 function showFormPopup() {
     document.getElementById('idgen-form').style.display = 'block';
     document.getElementById('blur').style.display = 'block';
@@ -7,3 +8,25 @@ function quitFormPopup() {
     document.getElementById('idgen-form').style.display = 'none';
     document.getElementById('blur').style.display = 'none';
 }
+
+function generate() {
+    const devname = document.getElementById('devname').value;
+    const projectname = document.getElementById('projectname').value;
+    
+    const id = 'OC-' + Math.random().toString(36).substr(2, 9).toUpperCase();
+    
+    alert(`Tu Orange ID Ha sido generado. Código: ${id}`);
+
+    const fileContent = `ID: ${id}\nName: ${devname}\nProject: ${projectname}`;
+    
+    const blob = new Blob([fileContent], { type: 'text/plain' });
+    
+    const link = document.createElement('a');
+    link.href = URL.createObjectURL(blob);
+    link.download = `${devname}-${projectname}-OrangeID.oid`;
+    
+    link.click();
+    
+    quitFormPopup();
+}
+*/
