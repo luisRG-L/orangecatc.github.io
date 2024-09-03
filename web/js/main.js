c9102b813c0ad85ccd46b67557af1d06b815c6fd
luisRG-L <122104739+luisRG-L@users.noreply.github.com>
2024-09-03 21:59:07 +0200

    Linking

--- web/js/main.js
+++ web/js/main.js
@@ -1,1 +1,8 @@
-document.querySelector('footer').innerHTML = document.querySelector("footer").innerHTML + `<p>&copy; Orange Cat</p>`;
+document.querySelector('footer').innerHTML = document.querySelector("footer").innerHTML + `
+<span>&copy; Orange Cat</span>
+<nav>
+    <a href="https://www.youtube.com/@bot_techjr" target="_blank">Youtube</a>
+    <a href="https://discord.gg/ThUb8mTdyA" target="_blank">Discord</a>
+    <a href="https://github.com/luisRG-L/Bot-tech-Jr" target="_blank">GitHub</a>
+</nav>
+`;
