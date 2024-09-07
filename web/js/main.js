138cbca261beda12dcf354052cf043d8372fc1ed
luisRG-L <122104739+luisRG-L@users.noreply.github.com>
2024-09-07 16:46:40 +0200

    Style-update

--- web/js/main.js
+++ web/js/main.js
@@ -1,8 +1,9 @@
 document.querySelector('footer').innerHTML = document.querySelector("footer").innerHTML + `
-<span>&copy; Orange Cat</span>
+<span>Orange Cat / 2024</span>
 <nav>
     <a href="https://www.youtube.com/@bot_techjr" target="_blank">Youtube</a>
     <a href="https://discord.gg/ThUb8mTdyA" target="_blank">Discord</a>
     <a href="https://github.com/luisRG-L/Bot-tech-Jr" target="_blank">GitHub</a>
 </nav>
+<span style="font-size: 0.7em">Hecho con HTML, CSS y JS nativo</span>
 `;
